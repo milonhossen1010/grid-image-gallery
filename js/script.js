baac8a77775d1fb9dd0413d5ca5a9f6572d8515b
milon <milonhossen1010@gmail.com>
2023-04-08 05:44:06 +0600

    Make mobile responsive

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
-fetch("https://picsum.photos/v2/list?page=2&limit=23")
+fetch("https://picsum.photos/v2/list?limit=23")
         .then((response) => response.json())
         .then((data) => {
             let html = '';
     data.map(data => {
+        if(data.width <= 2500){
         let htmlSegment = `
         <div>
             <a data-lightbox="roadtrip" href="${data.download_url}">
                 <img  src="${data.download_url}" >
             </a>
+  
         </div>`;
 
         html += htmlSegment;
+    }
     });
 
     let container = document.querySelector('.container');
